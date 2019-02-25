2942250a378ee8a08f1bbb36ac2e53459b28b375
Harry Pan <harry.pan@intel.com>
2019-02-25 20:36:41 +0800

    BACKPORT: PM / sleep: Refactor filesystems sync to reduce duplication
    Create a common helper to sync filesystems for system suspend and
    hibernation.
    
    Signed-off-by: Harry Pan <harry.pan@intel.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    [ rjw: Changelog ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [kawaaii: Backport to K4.19]
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- kernel/power/user.c
+++ kernel/power/user.c
@@ -10,7 +10,6 @@
  */
 
 #include <linux/suspend.h>
-#include <linux/syscalls.h>
 #include <linux/reboot.h>
 #include <linux/string.h>
 #include <linux/device.h>
@@ -239,9 +238,7 @@ static long snapshot_ioctl(struct file *filp, unsigned int cmd,
 		if (data->frozen)
 			break;
 
-		printk("Syncing filesystems ... ");
-		ksys_sync();
-		printk("done.\n");
+		ksys_sync_helper();
 
 		error = freeze_processes();
 		if (error)
