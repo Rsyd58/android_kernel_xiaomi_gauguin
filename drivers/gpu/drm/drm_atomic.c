96231bf6b599047711fcf8a3f0d9dbbea93c4844
Carlotta-Montelli <carlotta.montelli.0201@gmail.com>
2025-09-16 09:41:50 +0000

    Revert "drm/drm_atomic: Boost only if last interaction was within 3s"
    This reverts commit 7a48526bdc40f8c16896881039b2cc0c4e938841.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2607,10 +2607,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) && time_before(jiffies, last_input_time + msecs_to_jiffies(3000))) {
-		cpu_input_boost_kick();
-	}
-
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
 
 	state = drm_atomic_state_alloc(dev);
