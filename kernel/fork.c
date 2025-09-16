05293601ea9102ec87a85e53bf59589b1ca905cc
Carlotta-Montelli <carlotta.montelli.0201@gmail.com>
2025-09-16 10:28:53 +0000

    Revert "kernel: Fixup miss merge"
    This reverts commit 96f3d46ef78f2e3383383b52c3288d65b00b086a.

--- kernel/fork.c
+++ kernel/fork.c
@@ -94,7 +94,6 @@
 #include <linux/livepatch.h>
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
-#include <linux/devfreq_boost.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
 #include <linux/cpuset.h>
@@ -2389,7 +2388,7 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	if (task_is_zygote(current)) {
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 
 	/*
